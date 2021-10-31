093b311e69452eeb105411f40cec98c21f8e169d
michael carini <snownkc@icloud.com>
2021-10-31 19:01:12 +0000

    finished landing page, started new page, obj shells created

---
+++ NASA-EMCS/NASA-EMCS/Views/MenuWindow01.xaml.cs
@@ -0,0 +1,44 @@
+﻿using NASA_EMCS_Logic.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NASA_EMCS.Views
+{
+    /// <summary>
+    /// Interaction logic for MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+
+        private ButtonOBJ _Button;
+        public MenuWindow()
+        {
+            InitializeComponent();
+            _Button = new ButtonOBJ();
+        }
+
+        private void EngageBTN(object sender, RoutedEventArgs e)
+        {
+            GameWindow GameWindow02 = new GameWindow();
+            GameWindow02.Show();
+            this.Close();
+
+        }
+
+        private void AbortBTN(object sender, RoutedEventArgs e)
+        {      
+            this.Close();
+        }
+    }
+}
---
+++ NASA-EMCS/NASA-EMCS_Logic/Objects/ButtonOBJ.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NASA_EMCS_Logic;
+
+namespace NASA_EMCS_Logic.Objects
+{
+    public class ButtonOBJ
+    {
+
+        
+    }
+}
